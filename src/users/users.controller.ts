cf4bf2c2f234a1693920e5ce696e8e9618bef2dc
rachealkuranchie <kuranchieracheal@gmail.com>
2024-07-18 18:41:33 +0000

    created the crud routes for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /**
+   * GET /users
+   * GET /users/:id
+   * POST /users
+   * PATCH /users/:id
+   * DELETE /users/:id
+   */
+
+  @Get() // /users
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get('interns') //GET /users/intern
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() //POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //GET /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
