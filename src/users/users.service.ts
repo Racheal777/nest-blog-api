195d5cf62ee2071296addb5ab5198bc67757b78e
rachealkuranchie <kuranchieracheal@gmail.com>
2024-07-18 20:09:32 +0000

    handled errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -24,13 +25,17 @@ export class UsersService {
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
-      return this.users.filter((user) => user.role === role);
+     const rolesArray =  this.users.filter((user) => user.role === role);
+
+     if(rolesArray.length === 0) throw new NotFoundException('User role not found')
+        return rolesArray
     }
     return this.users;
   }
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
+    if(!user) throw new NotFoundException('User not found')
     return user;
   }
 
